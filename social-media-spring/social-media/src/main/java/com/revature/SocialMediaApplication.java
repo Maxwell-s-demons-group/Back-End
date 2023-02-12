003ecf3488ef2878ffeab207c436c48d05e86812
LwFrahm <luke.2017@hotmail.com>
2023-02-11 22:47:18 -0600

    Finished updates

--- social-media-spring/social-media/src/main/java/com/revature/SocialMediaApplication.java
+++ social-media-spring/social-media/src/main/java/com/revature/SocialMediaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:dev.properties")
+@PropertySource("dev.properties") // classpath:dev
 public class SocialMediaApplication {
 
 	public static void main(String[] args) {
